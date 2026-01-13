88953627aa3f7095762b074f0c1b3bead09fe765
Kevina <kevina585@gmail.com>
2026-01-12 19:30:22 -0500

    updated menu options with new methods

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
 public class Main {
-  public static void main(String[] args) {
+  static void main(String[] args) {
     System.out.println("Weather App Starting...");
 
     WeatherApp app = new WeatherApp();
--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -21,33 +21,111 @@ public class WeatherApp{
 
             switch (choice){
                 case "1":
-                    System.out.println("Search");
+                    SearchWeather();
                     break;
                 case "2":
-                    System.out.println("Fav");
+                    updateList();
                     break;
                 case "3":
-                    System.out.println("List");
+                    listFavorites();
                     break;
                 case "4":
-                    System.out.println("Update");
-                    break;
-                case "5":
                     running = false;
                     System.out.println("Goodbye!");
                     break;
                 default:
                     System.out.println("Invalid choice. Please Try Again.");
+                    //Pause for user
+                    try { Thread.sleep(1000); } catch (InterruptedException e){}
             }
         }
+        scanner.close();
     }
 
     private void showMenu(){
         System.out.println("          MAIN MENU\n");
         System.out.println("1. Select City");
-        System.out.println("2. Add to favorites");
+        System.out.println("2. Manage favorites");
         System.out.println("3. List favorites");
-        System.out.println("4. Update Favorites");
-        System.out.println("5. Exit App");
+        System.out.println("4. Exit App");
     }
+
+    private void SearchWeather(){
+
+    }
+    private void updateList(){
+        System.out.println("1. Add City");
+        System.out.println("2. Remove a city");
+        System.out.println("3. Return to menu");
+
+        String choice = scanner.nextLine();
+
+        switch(choice){
+            case "1":
+                addtoFav();
+                break;
+            case "2":
+                removefromFav();
+                break;
+            case "3":
+                System.out.println("Returning to main");
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+                //Pause for user
+                try { Thread.sleep(1000); } catch (InterruptedException e){}
+        }
+    }
+
+    private void addtoFav(){
+        System.out.println("\n Enter City to add: ");
+        String city = scanner.nextLine();
+
+        if(favorites.size() >= 3){
+            System.out.println("*** Maximum number of favorites added ***");
+        }else if(favorites.contains(city)){
+            System.out.println("***" + city + " is already in favorites. ");
+        }else {
+            favorites.add(city);
+            System.out.println(city + " added to favorites list");
+        }
+    }
+    private void removefromFav(){
+        //Different error message than listFavorites()
+        if(favorites.isEmpty()) {
+            System.out.println("No favorites to remove.");
+            return;
+        }
+
+        System.out.println("Select number to remove: \n");
+        listFavorites();
+
+        try {
+            int index = Integer.parseInt(scanner.nextLine()) - 1;
+
+            if(index >= 0 && index < favorites.size()){
+                String removed = favorites.remove(index);
+                System.out.println("Removed: " + removed);
+            }else {
+                System.out.println("Invalid number");
+            }
+        } catch (NumberFormatException e){
+            System.out.println("Please enter a valid number");
+        }
+
+    }
+
+    private void listFavorites(){
+        System.out.println("       YOUR FAVORITE CITIES");
+
+        if(favorites.isEmpty()) {
+            System.out.println("No favorites yet! Please try again");
+        }else{
+            for(int i = 0; i < favorites.size(); i++){
+                System.out.println((i + 1) + ". " + favorites.get(i));
+            //Add weather details
+            }
+        }
+    }
+
 }
