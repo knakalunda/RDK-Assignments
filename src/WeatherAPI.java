af5ef9d15c4651ed4ff6ac3263b86d4bd94652c5
Kevina <kevina585@gmail.com>
2026-01-13 02:19:22 -0500

    small bug fixes

--- src/WeatherAPI.java
+++ src/WeatherAPI.java
@@ -2,9 +2,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Locale;
-
-import static javax.swing.UIManager.getString;
 
 public class WeatherAPI {
     private final String apiKey;
